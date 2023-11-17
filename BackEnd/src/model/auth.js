b0ea0efb3588feaae8d05b22e5a529fb21a1c4ea
nam <nam@gmail.com>
2023-11-17 13:18:40 +0700

    fix bug

--- BackEnd/src/model/auth.js
+++ BackEnd/src/model/auth.js
@@ -1,25 +1,30 @@
 import mongoose from "mongoose";
 
 const authSchame = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    images: {
-        type: String,
-    },
-    role:{
-        type: String,
-        default:"member",
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "member",
+  },
+});
 
-export default mongoose.model("Auth",authSchame)
+export default mongoose.model("Auth", authSchame);
